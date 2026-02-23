28e34605d764420459ddf1cae1a6c640e1a13dc4
Jeffrey Kidd <jmkidd@umich.edu>
2026-02-23 13:46:30 -0500

    initial

---
+++ refMEUtils.py
@@ -0,0 +1,122 @@
+# some general functions
+import subprocess
+
+
+###############################################################################
+def read_fasta_file_to_dict(fastaFile):
+    myDict = {}
+    inFile = open(fastaFile,'r')
+    line = inFile.readline()
+    line = line.rstrip()
+    if line[0] != '>':
+        print('ERROR, FILE DOESNNOT START WITH >')
+        sys.exit()
+    myName = line[1:].split()[0]  # ignore other info
+    myDict[myName] = {}
+    myDict[myName]['seq'] = ''
+    myDict[myName]['seqLen'] = 0
+    mySeq = ''
+    while True:
+        line = inFile.readline()
+        if line == '':
+            myDict[myName]['seq'] = mySeq
+            myDict[myName]['seqLen'] = len(myDict[myName]['seq'])
+            break
+        line = line.rstrip()
+        if line[0] == '>':
+            myDict[myName]['seq'] = mySeq
+            myDict[myName]['seqLen'] = len(myDict[myName]['seq'])
+            myName = line[1:].split()[0]  # ignore other info
+            myDict[myName] = {}
+            myDict[myName]['seq'] = ''
+            myDict[myName]['seqLen'] = 0
+            mySeq = ''
+            continue
+        mySeq += line
+    inFile.close()
+    return myDict
+###############################################################################
+###############################################################################
+def add_breaks_to_line(seq,n=50):
+    myList = []
+    myList = [i for i in seq]
+    newList = []
+    c = 0
+    for i in myList:
+        newList.append(i)
+        c += 1
+        if c % n == 0 and c != (len(myList)):
+            newList.append('\n')
+    myStr = ''.join(newList)
+    return myStr    
+###############################################################################
+###############################################################################
+# Helper function to run commands,
+# doesn't check return or print to log.  Use for 'grep' so that doesn't
+# fail if no items found
+def runCMDNoFail(cmd):
+    val = subprocess.Popen(cmd, shell=True).wait()
+###############################################################################
+# Helper function to run commands, handle return values and print to log file
+def runCMD(cmd):
+    val = subprocess.Popen(cmd, shell=True).wait()
+    if val == 0:
+        pass
+    else:
+        print('command failed')
+        print(cmd)
+        sys.exit(1)
+###############################################################################
+# Helper function to run commands, handle return values and print to log file
+def runCMD_output(cmd):
+    val = subprocess.Popen(cmd, text=True, shell=True, stdout = subprocess.PIPE)
+    resLines = []
+    for i in val.stdout:
+       i = i.rstrip()
+       resLines.append(i)
+    return resLines
+#############################################################################        
+# Helper function to read in information from genome .fai file and return
+# a dictionary containing chrom names and lengths
+def read_chrom_len(faiFileName):
+    chromLens = {}
+    inFile = open(faiFileName,'r')
+    for line in inFile:
+        line = line.rstrip()
+        line = line.split()
+        chromLens[line[0]] = int(line[1])
+    inFile.close()
+    return chromLens    
+############################################################################# 
+##############################################################################
+# Returns complement of a bp.  If not ACGT then return same char
+def complement(c):
+    if c == 'A':
+        return 'T'
+    if c == 'T':
+        return 'A'
+    if c == 'C':
+        return 'G'
+    if c == 'G':
+        return 'C'
+    if c == 'a':
+        return 't'
+    if c == 't':
+        return 'a'
+    if c == 'c':
+        return 'g'
+    if c == 'g':
+        return 'c'
+    # If not ACTGactg simply return same character
+    return c   
+##############################################################################
+# Returns the reverse compliment of sequence 
+def revcomp(seq):
+    c = ''    
+    seq = seq[::-1] #reverse
+    # Note, this good be greatly sped up using list operations
+    seq = [complement(i) for i in seq]
+    c = ''.join(seq)
+    return c
+##############################################################################
+
